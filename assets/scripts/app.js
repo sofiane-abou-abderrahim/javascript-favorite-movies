f9e9c9b552b2f42922be5cb10096e38665dcf95f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-15 18:05:29 +0300

    updated app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,3 @@
-// const header = document.querySelector('header');
-
-// const addMovieButton = header.querySelector('button');
-
-// const modal = document.getElementById('add-modal');
-
-// addMovieButton.addEventListener('click', () => {
-//   modal.className = 'visible';
-// });
-
 const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
