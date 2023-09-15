39cdaea7d7fb3eb6a15650fdf77f673026621e4b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-15 17:56:37 +0300

    selected modal and add button

---
+++ assets/scripts/app.js
@@ -0,0 +1,15 @@
+// const header = document.querySelector('header');
+
+// const addMovieButton = header.querySelector('button');
+
+// const modal = document.getElementById('add-modal');
+
+// addMovieButton.addEventListener('click', () => {
+//   modal.className = 'visible';
+// });
+
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
