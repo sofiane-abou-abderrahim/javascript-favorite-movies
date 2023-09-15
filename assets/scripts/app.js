c0ba3cff5a63e61c8d7940ed11ce9575dce90494
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-15 18:46:55 +0300

    selected backdrop & created toggleBackdrop function and called it in toggleMovieModal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,19 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
 
 const toggleMovieModal = () => {
   // function () {}
   // addMovieModal.className = 'modal card visible';
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop(); // instead of adding another startAddMovieButton event listener
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+// startAddMovieButton.addEventListener('click', toggleBackdrop); // called toggleBackdrop function inside toggleMovieModal instead
